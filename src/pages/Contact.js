997db66a5f123638e5b32a651bed81871475209e
DevAntonioRogers <ar919061493@gmail.com>
2023-02-24 01:46:53 -0500

    Worked on Contact Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,24 +2,43 @@ import '../components/Hero/ContactHero.css'
 
 function Contact() {
   return (
-    <div className='contact-hero-container'>
-      <div className="contact-hero-inner">
-        <div className='flex-test'>
-          <div className='contact-hero-socials'>
-            <h1>SOCIALS</h1>
+    <>
+      <div className='contact-hero-container'>
+
+        <div className='contact-hero-inner'>
+          <div className='social'>
+            <h3>Contact Info</h3>
+            <div className='contact-social-info'>
+              <i class="fa-solid fa-phone"></i>
+              <span>1800-435-2343</span>
+            </div>
+            <div className='contact-social-info'>
+              <i class="fa-solid fa-envelope-open"></i>
+              <span>contact@tripper.com</span>
+            </div>
+            <div className='contact-social-info'>
+              <i class="fa-solid fa-building"></i>
+              <span>1343 AntonioRogers Rd</span>
+            </div>
+            <div className='contact-social-box'>
+              <i class="fa-brands fa-twitter"></i>
+              <i class="fa-brands fa-instagram"></i>
+              <i class="fa-brands fa-facebook"></i>
+              <i class="fa-brands fa-snapchat"></i>
+            </div>
           </div>
 
-          <div className='contact-hero-map'>
+          <div className='map'>
             <h1>MAP</h1>
           </div>
-        </div>
 
-        <div className='contact-hero-form'>
-          <h1>FORM</h1>
+
+          <div className='form'>
+            <h1>FORM</h1>
+          </div>
         </div>
       </div>
-    </div>
-
+    </>
   )
 }
 
