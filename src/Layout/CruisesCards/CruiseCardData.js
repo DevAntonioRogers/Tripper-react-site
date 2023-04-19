db0d7608a37c24b483c1471648a060df848490e9
DevAntonioRogers <ar919061493@gmail.com>
2023-04-19 08:19:08 -0400

    Cruise Details Page

--- src/Layout/CruisesCards/CruiseCardData.js
+++ src/Layout/CruisesCards/CruiseCardData.js
@@ -21,7 +21,13 @@ export const CruiseCardData = [
     arcade: false,
     spa: true,
     roomService: true,
-    smokingRooms: false
+    smokingRooms: false,
+    moreImages: [
+      "https://images.unsplash.com/photo-1561811358-21aef14f0551?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+      'https://images.unsplash.com/photo-1617859047452-8510bcf207fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      'https://images.unsplash.com/photo-1610641818989-c2051b5e2cfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      'https://images.unsplash.com/photo-1540541338287-41700207dee6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+    ]
   },
 
   {
--- src/pages/CruiseDetails.js
+++ src/pages/CruiseDetails.js
@@ -1,16 +1,43 @@
 import { CruiseCardData } from "../Layout/CruisesCards/CruiseCardData";
 import { useParams } from "react-router-dom";
+import { useState } from 'react'
+
+import './CruiseDetails.css'
 
 const CruiseDetails = () => {
+
+  const [currentImage, setCurrentImage] = useState(0)
+
   const { name } = useParams();
   const cruise = CruiseCardData.find(cruise => cruise.name.replace(/\s/g, '') === name);
-  return <div>
-    {name}
 
-    <div>
 
+  return (
+    <div className="cruiseDetails">
+      <div className="leftSide">
+        <div className="mainImg">
+          <img src={cruise.moreImages[currentImage]} alt='' />
+        </div>
+
+        <div className="moreImg">
+          <img src={cruise.moreImages[0]} onClick={e => setCurrentImage(0)} alt='' />
+          <img src={cruise.moreImages[1]} onClick={e => setCurrentImage(1)} alt='' />
+          <img src={cruise.moreImages[2]} onClick={e => setCurrentImage(2)} alt='' />
+          <img src={cruise.moreImages[3]} onClick={e => setCurrentImage(3)} alt='' />
+        </div>
+      </div>
+
+
+      <div className="rightSide">
+        <div className="cruiseName">
+          <h1>{cruise.name}</h1>
+        </div>
+        <div>
+          {cruise.details}
+        </div>
+      </div>
     </div>
-  </div>;
+  )
 };
 
 export default CruiseDetails;
